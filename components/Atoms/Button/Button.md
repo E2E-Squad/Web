83cf86008d0a3fa1357a29de67b2b12c8be6b8a7
loichu <loichumbert5450@gmail.com>
2022-02-02 00:14:56 +0100

    Example component ok

--- components/Atoms/Button/Button.md
+++ components/Atoms/Button/Button.md
@@ -1,6 +1,5 @@
 This button is really basic.
 
-Button example:
 ```tsx
-<Button text="click">Click me</Button>
+<Button />
 ```
--- components/Atoms/Button/Button.tsx
+++ components/Atoms/Button/Button.tsx
@@ -1,48 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './Button.module.scss';
-import classNames from "classnames";
-
-interface ICounter {
-    value: number;
-    increment?: () => number;
-}
-
-interface Props {
-    /** Text to be displayed before the button */
-    text?: string,
-    children: React.ReactChild
-}
 
 /**
  * The only true button.
- *
- * @author [Loïc Humbert](https://github.com/loichu)
  */
-const Button = ({text, children}: Props) => {
-    // Type is inferred by the initial value.
-    const [clicked, setClicked] = useState(false)
-    const [counter, setCounter] = useState<ICounter>({ value: 0 })
-    counter.increment = () => {
-        console.log("click")
-        setCounter({value: counter.value + 1})
-        console.log(counter.value)
-        return counter.value
-    }
-    const classes = classNames(styles.btnDummy, styles['btn-margin'], {
-        [styles.red]: clicked
-    })
+const Button = () => {
+    // styles
     return (
-        <>
-            <span style={{ color: 'blue' }}>{text}</span>
-            <span>{counter.value}</span>
-            <button className={classes} onClick={() => setClicked(!clicked)}>{children}</button>
-            <button className={classes} onClick={counter.increment}>Increment counter</button>
-            <style jsx>{`
-              span {
-                margin: 8px;
-              }
-            `}</style>
-        </>
+        <button className={styles.red}>I am a button</button>
     )
 }
 
