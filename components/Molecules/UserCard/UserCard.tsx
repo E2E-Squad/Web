2e46e4e7e2fc8319a512db3183ab76f684cd85a7
Jean-Christio Martin <admin@marjc.ch>
2022-02-03 22:05:43 +0100

    Update card details

--- components/Molecules/UserCard/UserCard.tsx
+++ components/Molecules/UserCard/UserCard.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactStars from "react-rating-stars-component";
 
+// Components
+import Button from '../../Atoms/Button/Button';
+
 // Style
 import style from "./UserCard.module.scss";
 
@@ -9,32 +12,45 @@ interface types {
     styleClass?: string,
     pseudo: string,
     ratingValue: number,
+    location?: string,
+    rank?: string,
 }
 
 const UserCard = (props: types) => {
     return (
         <div className={style[`${props.styleClass}`]}>
-           <div className={style[`user-card-info`]}>
-            <div className={style[`user-card-avatar`]}>
-                <div className={style[`user-card-avatar__container`]}></div>
+            <div className={style[`user-card-top`]}>
+                <div className={style[`user-card-info`]}>
+                    <div className={style[`user-card-avatar`]}>
+                        <div className={style[`user-card-avatar__container`]}></div>
+                    </div>
+                    <div className={style[`user-card-details`]}>
+                        <p className={style[`user-card-details__pseudo`]}>{props.pseudo}</p>
+                        <ReactStars
+                            count={5}
+                            size={18}
+                            value={props.ratingValue}
+                            isHalf={true}
+                            emptyIcon={<i className="far fa-star"></i>}
+                            halfIcon={<i className="fa fa-star-half-alt"></i>}
+                            fullIcon={<i className="fa fa-star"></i>}
+                            activeColor="#ffd700"
+                        />
+                    </div>
+                </div>
+                <div className={style[`user-card-btn`]}>
+                    {props.children}
+                </div>
             </div>
-            <div className={style[`user-card-details`]}>
-                <p className={style[`user-card-details__pseudo`]}>{props.pseudo}</p>
-                <ReactStars
-                    count={5}
-                    size={18}
-                    value={props.ratingValue}
-                    isHalf={true}
-                    emptyIcon={<i className="far fa-star"></i>}
-                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                    fullIcon={<i className="fa fa-star"></i>}
-                    activeColor="#ffd700"
-                />
+            <div className={style[`user-card-bottom`]}>
+                <div className={style[`user-card-profile`]}>
+                    <p>Classe: {props.rank}</p>
+                    <p>Localité: {props.location}</p>
+                </div>
+                <Button styleClass="btn">
+                    <p>Notifier</p>
+                </Button>
             </div>
-           </div>
-           <div className={style[`user-card-btn`]}>
-            {props.children}
-           </div>
         </div>
     )
 }
