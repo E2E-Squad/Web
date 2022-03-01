78bcd42abad572066f1801f9201400958e8cafc3
loichu <loic@humbert-web.dev>
2022-03-01 13:56:14 +0100

    Capitalize Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import React from 'react';
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
